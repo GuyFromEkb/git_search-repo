b3e8c75168aac95dff42ef40ff6ff8debad52153
Sergey <graygmai1@gmail.com>
2022-07-06 22:00:34 +0500

    отображения блоков, только при запросе

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', () => {
     let search = "";
-    // https://api.github.com/search/repositories?q=w
+
 
     //form
     document.querySelector('form').addEventListener('submit', (e) => {
@@ -9,15 +9,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const formData = new FormData(e.target);
         search = Object.fromEntries(formData.entries()).repo;
+        let counterSecondResult = 0;
 
         getData(search)
             .then((data) => {
                 Clean();
                 data.forEach(item => {
 
-                    if (item.name == search) { new repo(item).renderRepo(); }
-                    // console.log(item.name, item['html_url'], item.owner['avatar_url'], item.owner['login'], item['created_at'], item['watchers_count'], item['forks_count']);
-                    // renderRepo();
+                    if (searchResult(item.name, search)) {
+
+                        new repo(item).renderRepo(true);
+
+                    } else {
+
+                        if (counterSecondResult < 10 && searchSecondResult(item.name, search)) {
+
+                            new repo(item).renderRepo(false);
+                            counterSecondResult++;
+
+                        }
+                    }
                 });
 
             })
@@ -26,11 +37,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (document.querySelector('.result').children.length == 0) {
                     renderNoResult();
                 }
-                // console.log(document.querySelector('.result').children.length)
-            })
-            // if (document.querySelector('.result').children.length == 0) {
-            //     renderNoResult();
-            // }
+
+
+            });
+
 
 
 
@@ -43,6 +53,28 @@ document.addEventListener('DOMContentLoaded', () => {
         const data = await request.json();
 
         return data.items;
+    }
+
+    //Проверка на совпадения именни реп
+    function searchResult(repoName, search) {
+
+        return repoName.toLowerCase() == search.toLowerCase().replace(" ", "-");
+    }
+
+    //Проверка на совпадения строки в репозитории
+    function searchSecondResult(repoName, search) {
+
+
+        const cleanSearch = search.toLowerCase()
+            .split(/[-__\s]/)
+            .join("");
+
+        const cleanRepoName = repoName.toLowerCase()
+            .split(/[-__\s]/)
+            .join("");
+
+        return cleanRepoName.includes(cleanSearch);
+
     }
 
 
@@ -63,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
         //отрисовка  на стр
-        renderRepo() {
+        renderRepo(flag) {
 
             const date = this.date.substring(0, 10);
 
@@ -130,12 +162,17 @@ document.addEventListener('DOMContentLoaded', () => {
             </div>
             `;
 
-            document.querySelector('.result').insertAdjacentHTML('beforeend', item);
+            if (flag) {
+                document.querySelector('.result').insertAdjacentHTML('beforeend', item);
+            } else {
+                document.querySelector('.second-result__wrap').insertAdjacentHTML('beforeend', item);
+            }
+
         }
 
     }
 
-
+    //поиск не удался
     function renderNoResult() {
 
         const item =
@@ -150,12 +187,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
-
+    //очистка окон
     function Clean() {
 
-        const children = Array.from(document.querySelector('.result').children);
+        const resultChildren = Array.from(document.querySelector('.result').children);
+        const secResultChildren = Array.from(document.querySelector('.second-result__wrap').children);
 
-        children.forEach(item => {
+        resultChildren.forEach(item => {
+            item.remove();
+        });
+        secResultChildren.forEach(item => {
             item.remove();
         });
 
@@ -164,97 +205,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-    function renderRepo() {
-
-        const wrap = document.querySelector('.result');
-
-        const repoItem =
-            `
-            <div class="item">
-
-                <h4>
-                    <a class="rep-name" href="">Test Task</a>
-                </h4>
-
-                <img class="rep-img item__img" src="https://avatars.githubusercontent.com/u/50860624?v=4" alt="#">
-
-                <div class="item__info">
-
-                    <div class="item__owner-login">
-                        <p>Логин владельца: </p>
-                        <span class="rep-owner">katrendev</span>
-                    </div>
-
-                    <div class="item__search-name">
-
-                        <p>Используемые языки:</p>
-                        <span class="rep-lang">
-                            C# JavaScript
-                        </span>
-
-                    </div>
-
-                </div>
-
-                <div class="item__icon">
-                    <div class="item__watch">
-                        <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-eye mr-2">
-                            <path fill-rule="evenodd" d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z"></path>
-                        </svg>
-                        <span class="rep-watch">7</span> watching
-                    </div>
-
-
-                    <div class="item__fork">
-                        <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-repo-forked mr-2">
-                            <path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
-                        </svg>
-                        <span class="rep-fork">80</span> forks
-                    </div>
-
-                </div>
-
-            </div>
-            `;
-
-        wrap.insertAdjacentHTML('beforeend', repoItem);
-
-    }
-
-
-
-
-
-
-
-
-
-
-    // document.querySelector('.btn').addEventListener('click', () => {
-
-    //     fetch('https://api.github.com/search/repositories?q=testTask')
-    //         .then(data => data.json())
-    //         .then(data => {
-    //             data.items.forEach(element => {
-    //                 console.log(element);
-    //             });
-    //         });
-
-
-
-    //     console.log('click');
-
-
-    // });
-
-
-    // async function getData(url) {
-    //     const request = await fetch(url);
-    //     const data = await request.text();
-
-    //     return JSON.parse(data);
-    // }
 
 
 });
